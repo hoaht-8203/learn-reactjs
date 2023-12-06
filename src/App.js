622faf534ade27ecfddcc29d12d5f66b79627d2b
hoaht-8203 <hoahthe172735@gmail.com>
2023-12-06 19:27:13 +0700

    commit 3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Author: hoang hoa
         </a>
       </header>
     </div>
