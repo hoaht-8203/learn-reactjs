6b134ca8432dc4f6c2b0b935a242ab3cb147563b
hoaht-8203 <hoahthe172735@gmail.com>
2023-12-06 19:20:18 +0700

    commit 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hoang Trung Hoa 1 cau gi do
+          Hoang Trung Hoa
         </p>
         <a
           className="App-link"
